a87463dfc96590ad50074961b27de7e5def472d9
Marquis Wong <marquis@w-ng.com>
2023-09-27 21:14:57 -0500

    Reformat file

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ PUSHCUT_API_TOKEN = os.environ['PUSHCUT_API_TOKEN']
 PUSHCUTS_TRACKING_SHORTCUT = "Time Tracking"
 PUSHCUTS_NOT_TRACKING_SHORTCUT = "Start Tracking"
 
+
 def get_projects():
     json = toggl_api('projects')
     return json
+
 
 def get_current_time_entry():
     json = toggl_api('time_entries/current')
@@ -34,6 +36,7 @@ def get_current_time_entry():
     json['duration_formatted'] = duration
     return json
 
+
 def get_last_time_entry():
     json = toggl_api('time_entries')[0]
 
@@ -49,19 +52,21 @@ def toggl_api(endpoint):
     r = requests.get(f"https://api.track.toggl.com/api/v9/me/{endpoint}", auth=(TOGGL_API_TOKEN, 'api_token'))
     return r.json()
 
+
 def pushcuts_post(shortcut, title, text):
-    r = requests.post(f"https://api.pushcut.io/{PUSHCUT_API_TOKEN}/notifications/{shortcut}", json={'title': title, 'text': text})
+    r = requests.post(f"https://api.pushcut.io/{PUSHCUT_API_TOKEN}/notifications/{shortcut}",
+                      json={'title': title, 'text': text})
     return r.json()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     current_time_entry = get_current_time_entry()
     if current_time_entry is None:
         last_time_entry = get_last_time_entry()
-        pushcuts_post(PUSHCUTS_NOT_TRACKING_SHORTCUT, f"Untracked Time", f"{last_time_entry['untracked_duration_formatted']}")
+        pushcuts_post(PUSHCUTS_NOT_TRACKING_SHORTCUT, f"Untracked Time",
+                      f"{last_time_entry['untracked_duration_formatted']}")
         print(last_time_entry)
     else:
-        pushcuts_post(PUSHCUTS_TRACKING_SHORTCUT, f"Tracking for {current_time_entry['duration_formatted']}", f"{current_time_entry['project_name']} - {current_time_entry['description']}")
+        pushcuts_post(PUSHCUTS_TRACKING_SHORTCUT, f"Tracking for {current_time_entry['duration_formatted']}",
+                      f"{current_time_entry['project_name']} - {current_time_entry['description']}")
         print(current_time_entry)
-
